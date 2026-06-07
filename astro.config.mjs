4803e0443963c0dc76c6ed06e2642c5cbfde136a
Bryce Lindsey <brycelindsey@live.com>
2026-06-06 23:46:20 -0700

    Updated node.js

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://brycel20102010.github.io',
+	base: '/engineering-curiosities',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
